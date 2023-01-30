72c644dc7532a1e9840de662b5068537559cc05d
Mayank Patel <patel_mayank17@yahoo.com>
2023-01-29 22:42:39 -0800

    added readme

--- pages.js
+++ pages.js
@@ -1,7 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/patelmayank17/patelmayank17.github.io.git";
-//"https://github.com/patelmayank17/Portfolio_ReactApp.git";
+const repoURL = "https://github.com/patelmayank17/Portfolio_ReactApp.git"
 
 ghpages.publish(
   pathname,
